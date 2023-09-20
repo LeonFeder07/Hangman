5e0419d50d1149a207133d7e2163fa66dfeaabc2
LeonFeder07 <leonfeder18@gmail.com>
2023-09-20 20:05:43 +0200

    ddwadwa

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -12,23 +12,59 @@ public class hangman {
     private JTextField b1;
     private JButton check;
     private JLabel h;
-    private JTextField textField1;
+    private JTextField versuchefeld;
 
 
     public hangman() {
+        String wort ="Halllo";
+        String[] buchstabe;
+        buchstabe = new String [wort.length()];
         knopf.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               String wort ="hallo";
+
+                b1.setText("");
                for(int i=0;i<wort.length();i++){
-                   b1.setText(b1.getText()+" _ ");
+
+                  buchstabe[i]= "_";
+                  b1.setText(b1.getText()+" " +buchstabe[i]);
+                   versuchefeld.setText("");
                }
+
             }
         });
         check.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String versuch= buchstabenchecker.getText();
+                String versuchgroß=versuch.toUpperCase();
+                String versuchklein=versuch.toLowerCase();
+                if(wort.indexOf(versuchgroß)==-1 && wort.indexOf(versuchklein)==-1){
+                    versuchefeld.setText(versuchefeld.getText()+" "+versuch);
+                }
+                if(versuch.length()==1) {
+                    for (int i = 0; i < wort.length(); i++) {
+                        String wortplatzhalter = String.valueOf(wort.charAt(i));
+                        if (wortplatzhalter.equals(versuchklein)) {
+                            buchstabe[i] = versuchklein;
+
+                        }
+                        if (wortplatzhalter.equals(versuchgroß)) {
+                            buchstabe[i] = versuchgroß;
+
+                        }
+
+                    }
+                }
+
+
+                b1.setText("");
+                for(int i=0;i<wort.length();i++){
+
+                    b1.setText(b1.getText()+" " +buchstabe[i]);
+                    buchstabenchecker.setText("");
+                }
             }
         });
     }
@@ -41,4 +77,7 @@ public class hangman {
         frame.setVisible(true);
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
