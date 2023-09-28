9fe4f12b18bb2dbe8dfe6e08ed0271604336b6e5
leon.feder <leonfeder18@gmail.com>
2023-09-28 09:42:16 +0200

    wda

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -21,81 +21,71 @@ public class hangman {
     private JLabel h;
     private JTextField versuchefeld;
     private JLabel label;
-    int versuchsanzahl=0;
+    int versuchsanzahl = 0;
     String wort;
     String[] buchstabe;
-    int fehleranzahl=0;
+    int fehleranzahl = 0;
 
 
     public hangman() throws IOException {
         BufferedReader ausleser = new BufferedReader(new FileReader("src/hangman/wortliste.txt"));
 
-        String ausgesuchteswort="";
 
-
-        try {
-            ausgesuchteswort= ausleser.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        String finalAusgesuchteswort = ausgesuchteswort;
-
-         wort =ausgesuchteswort;
+        wort = "";
 
 
         knopf.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(finalAusgesuchteswort);
-                int zeile=0;
-                double zufallszahld=Math.random()*100000+5*Math.random()*200-10*Math.random()*100;
-                int zufallszahl =(int) zufallszahld;
 
-                       try {
-                           for (String aktuellesWort = ausleser.readLine(); aktuellesWort != null; aktuellesWort = ausleser.readLine()) {
-                               while (zeile != zufallszahl) {
-                                   zeile = zeile + 1;
-                                   wort = ausleser.readLine();
+                int zeile = 0;
+                double zufallszahld = Math.random() * 120000 + 5 * Math.random() * 200 - 10 * Math.random() * 100;
+                int zufallszahl = (int) zufallszahld;
 
-                               }}
-                           } catch(IOException ex){
-                               throw new RuntimeException(ex);
-                           }
-                buchstabe = new String [wort.length()];
+                try {
+                    for (String aktuellesWort = ausleser.readLine(); aktuellesWort != null; aktuellesWort = ausleser.readLine()) {
+                        while (zeile != zufallszahl) {
+                            zeile = zeile + 1;
+                            wort = ausleser.readLine();
 
+                        }
+                    }
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                buchstabe = new String[wort.length()];
 
 
                 b1.setText("");
-               for(int i=0;i<wort.length();i++){
+                for (int i = 0; i < wort.length(); i++) {
 
-                  buchstabe[i]= "_";
-                  b1.setText(b1.getText()+" " +buchstabe[i]);
-                   versuchefeld.setText("");
-                   buchstabenchecker.setText("");
-                   versuchsanzahl=0;
-                   zeile=0;
-                   zufallszahl=0;
-                   fehleranzahl=0;
-               }
+                    buchstabe[i] = "_";
+                    b1.setText(b1.getText() + " " + buchstabe[i]);
+                    versuchefeld.setText("");
+                    buchstabenchecker.setText("");
+                    versuchsanzahl = 0;
+                    zeile = 0;
+                    zufallszahl = 0;
+                    fehleranzahl = 0;
+                }
 
             }
         });
         check.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                    versuchsanzahl=versuchsanzahl+1;
-                String versuch= buchstabenchecker.getText();
-                String versuchgroß=versuch.toUpperCase();
-                String versuchklein=versuch.toLowerCase();
-                if(versuchklein.equals(wort.toLowerCase())) {
+                versuchsanzahl = versuchsanzahl + 1;
+                String versuch = buchstabenchecker.getText();
+                String versuchgroß = versuch.toUpperCase();
+                String versuchklein = versuch.toLowerCase();
+                if (versuchklein.equals(wort.toLowerCase())) {
                     b1.setText(wort);
-                    buchstabenchecker.setText("Du hast nur "+versuchsanzahl+" Versuch(e) gebraucht");
+                    buchstabenchecker.setText("Du hast nur " + versuchsanzahl + " Versuch(e) gebraucht");
 
-                }else {
+                } else {
                     if (wort.indexOf(versuchgroß) == -1 && wort.indexOf(versuchklein) == -1) {
                         String platzhalter2 = versuchefeld.getText();
-                         fehleranzahl=fehleranzahl +1;
+                        fehleranzahl = fehleranzahl + 1;
                         if (!platzhalter2.equals("")) {
                             versuchefeld.setText(versuchefeld.getText() + "," + versuch);
                         } else {
@@ -121,7 +111,6 @@ public class hangman {
                     }
 
 
-
                     b1.setText("");
 
                     for (int i = 0; i < wort.length(); i++) {
@@ -129,14 +118,14 @@ public class hangman {
                         b1.setText(b1.getText() + " " + buchstabe[i]);
                         buchstabenchecker.setText("");
                     }
-                    if(b1.getText().indexOf("_")==-1){
+                    if (b1.getText().indexOf("_") == -1) {
                         b1.setText(wort);
-                        buchstabenchecker.setText("Du hast nur "+versuchsanzahl+" Versuch(e) gebraucht");
+                        buchstabenchecker.setText("Du hast nur " + versuchsanzahl + " Versuch(e) gebraucht");
 
                     }
                 }
-                if(versuchefeld.getText().length()==19){
-                    buchstabenchecker.setText("Du hast "+ fehleranzahl +" Fehler gemacht. Probier es nochmal.");
+                if (versuchefeld.getText().length() == 19) {
+                    buchstabenchecker.setText("Du hast " + fehleranzahl + " Fehler gemacht. Probier es nochmal.");
                 }
 
 
@@ -144,7 +133,7 @@ public class hangman {
         });
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("hangman");
         frame.setContentPane(new hangman().panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
