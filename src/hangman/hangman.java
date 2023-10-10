8b095dbd1d932b7a56c1f536caec2eac8cab6d38
LeonFeder07 <leonfeder18@gmail.com>
2023-10-10 18:43:19 +0200

    mit duden und abfrage ob zu viele fehler gemacht wurde; hangman skizze fertig

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class hangman {
 
